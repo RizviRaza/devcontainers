ff156e7fd02ed0c62aa95731cdc374eccdcd7f4e
Syed Muhammad Raza Rizvi <smrazarizvi96@gmail.com>
2025-04-07 18:19:01 -0400

    ros2_nvidia_yolo_training_devcontainer uploaded

---
+++ ros2_nvidia_yolo_training_devcontainer/postCreateSetup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Source ROS2 setup
+source /opt/ros/humble/setup.bash
+
+echo "source /opt/ros/humble/setup.bash" >> ~/.bashrc
+echo "export FASTRTPS_DEFAULT_PROFILES_FILE=/ros2_ws/dds_config/config.xml" >> ~/.bashrc
+
+# Navigate to the workspace
+cd /ros2_ws
+
+rosdep install --from-paths src --ignore-src -r -y
+
+# Build the workspace
+# colcon build --symlink-install
+
+# Add any additional commands here
+# source install/setup.bash
